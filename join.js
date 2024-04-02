6e8773c877d75b7b486990372e63eeab57d3eae0
sujinRo <shtnwls1111@ewhain.net>
2024-04-02 13:34:35 +0900

    feat: 유효성 검사 통과 시, success alert

--- join.js
+++ join.js
@@ -6,6 +6,13 @@ const age = document.getElementById('age');
 const pwd = document.getElementById('pwd');
 const rePwd = document.getElementById('rePwd');
 
+//유효성 검사 여부
+let isName = false;
+let isEmail = false;
+let isAge = false;
+let isPwd = false;
+let isRePwd = false;
+
 //에러 or 성공
 const showError = (input, msg) => {
   const formControl = input.parentElement;
@@ -27,22 +34,30 @@ const isRequired = inputArr => {
     if (input.value.trim() == '') {
       if (input == name) {
         showError(input, '이름을 입력해주세요!');
+        isName = false;
       } else if (input.name == email) {
         showError(input, '이메일을 입력해주세요!');
+        isEmail = false;
       } else if (input == age) {
         showError(input, '나이를 입력해주세요!');
+        isAge = false;
       } else if (input.name == pwd) {
         showError(input, '비밀번호를 입력해주세요!');
+        isPwd = false;
       }
     } else {
       if (input == name) {
         showSuccess(input, '멋진 이름이네요!');
+        isName = true;
       } else if (input.name == email) {
         showSuccess(input, '올바른 이메일 형식입니다!');
+        isEmail = true;
       } else if (input == age) {
         showSuccess(input, '올바른 나이 형식입니다!');
+        isAge = true;
       } else if (input.name == pwd) {
         showSuccess(input, '올바른 비밀번호 형식입니다!');
+        isPwd = true;
       }
     }
   });
@@ -53,8 +68,10 @@ const isValidEmail = email => {
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(email.value)) {
     showSuccess(email, '올바른 이메일 형식입니다!');
+    isEmail = true;
   } else {
     showError(email, '올바른 이메일 형식이 아닙니다!');
+    isEmail = false;
   }
 };
 
@@ -62,10 +79,12 @@ const isValidAge = age => {
   const re = /^[1-9]\d*$/;
   if (age.value >= 19 && re.test(age.value)) {
     showSuccess(age, '올바른 나이 형식입니다!');
+    isAge = true;
   } else {
     if (age.value < 19) {
       showError(age, '19세 이상이어야 합니다!');
     } else showError(age, '올바른 나이 형식이 아닙니다!');
+    isAge = false;
   }
 };
 
@@ -73,16 +92,20 @@ const isValidPwd = pwd => {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{4,12}$/;
   if (re.test(pwd.value)) {
     showSuccess(pwd, '올바른 비밀번호입니다!');
+    isPwd = true;
   } else {
     showError(pwd, '올바른 비밀번호가 아닙니다!');
+    isPwd = false;
   }
 };
 
 const checkPwdMatch = (pwd, rePwd) => {
   if (pwd.value !== rePwd.value || pwd.value.trim() == '') {
     showError(rePwd, '비밀번호가 일치하지 않습니다.');
+    isRePwd = false;
   } else {
     showSuccess(rePwd, '비밀번호가 일치합니다');
+    isRePwd = true;
   }
 };
 
@@ -94,4 +117,8 @@ form.addEventListener('submit', e => {
   isValidAge(age);
   isValidPwd(pwd);
   checkPwdMatch(pwd, rePwd);
+
+  if (isName && isEmail && isAge && isPwd && isRePwd) {
+    alert('success');
+  }
 });
